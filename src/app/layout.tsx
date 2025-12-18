25a04307bfd1d3f5457cf8376ac9bdf3162b05a4
juanarangoceo <103976919+juanarangoceo@users.noreply.github.com>
2025-12-18 09:02:15 -0500

    feat: add meta pixel
    Added Facebook Pixel tracking script to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
@@ -33,6 +34,20 @@ export default function RootLayout({
             </head>
             <body className="font-sans antialiased text-coffee-900 bg-coffee-50" suppressHydrationWarning>
                 {children}
+                <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '830758678587204');
+            fbq('track', 'PageView');
+          `}
+        </Script>
             </body>
         </html>
     );
