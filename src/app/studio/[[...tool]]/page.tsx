455368b893df3a9da45030564fb548bba3748bdd
juanarangoceo <juanarangopm@gmail.com>
2025-12-26 19:04:40 -0500

    fix: build errors, move sanity.config to src, update moduleResolution

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import {NextStudio} from 'next-sanity/studio'
-import config from '../../../../sanity.config'
+import config from '../../../sanity.config'
 
 export const dynamic = 'force-static'
 
-export {metadata} from 'next-sanity/studio/metadata'
-export {viewport} from 'next-sanity/studio/viewport'
+// export {metadata} from 'next-sanity/studio/metadata'
+// export {viewport} from 'next-sanity/studio/viewport'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
